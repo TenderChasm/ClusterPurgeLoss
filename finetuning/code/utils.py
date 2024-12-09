cc3d5db4ab37edf2152532c27fb1e5d4f56e8869
TenderChasm <tenderchasm@gmail.com>
2024-12-09 22:50:42 +0100

    GGMA foreshadowing

--- finetuning/code/utils.py
+++ finetuning/code/utils.py
@@ -1,5 +1,24 @@
 import re
 
+from finetuning.code.external.DFG import DFG_java
+from tree_sitter import Language, Parser
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning) 
+
+dfg_function={
+    'java':DFG_java
+}
+#load parsers
+parsers={}        
+for lang in dfg_function:
+    LANGUAGE = Language('finetuning/libs/my-languages.so', lang)
+    parser = Parser()
+    parser.set_language(LANGUAGE) 
+    parser = [parser,dfg_function[lang]]    
+    parsers[lang]= parser
+
+
 def remove_comments_and_docstrings_java(source):
     def replacer(match):
         s = match.group(0)
@@ -16,3 +35,63 @@ def remove_comments_and_docstrings_java(source):
         if x.strip()!="":
             temp.append(x)
     return '\n'.join(temp)
+
+def tree_to_token_index(root_node):
+    if (len(root_node.children)==0 or root_node.type=='string') and root_node.type!='comment':
+        return [(root_node.start_point,root_node.end_point)]
+    else:
+        code_tokens=[]
+        for child in root_node.children:
+            code_tokens+=tree_to_token_index(child)
+        return code_tokens 
+
+def index_to_code_token(index,code):
+    start_point=index[0]
+    end_point=index[1]
+    if start_point[0]==end_point[0]:
+        s=code[start_point[0]][start_point[1]:end_point[1]]
+    else:
+        s=""
+        s+=code[start_point[0]][start_point[1]:]
+        for i in range(start_point[0]+1,end_point[0]):
+            s+=code[i]
+        s+=code[end_point[0]][:end_point[1]]   
+    return s
+
+def extract_dataflow(code, parser,lang):
+    #remove comments
+    try:
+        code=remove_comments_and_docstrings_java(code)
+    except:
+        pass    
+    #obtain dataflow
+    if lang=="php":
+        code="<?php"+code+"?>"    
+    try:
+        tree = parser[0].parse(bytes(code,'utf8'))    
+        root_node = tree.root_node  
+        tokens_index=tree_to_token_index(root_node)     
+        code=code.split('\n')
+        code_tokens=[index_to_code_token(x,code) for x in tokens_index]  
+        index_to_code={}
+        for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
+            index_to_code[index]=(idx,code)  
+        try:
+            DFG,_=parser[1](root_node,index_to_code,{}) 
+        except:
+            DFG=[]
+        DFG=sorted(DFG,key=lambda x:x[1])
+        indexs=set()
+        for d in DFG:
+            if len(d[-1])!=0:
+                indexs.add(d[1])
+            for x in d[-1]:
+                indexs.add(x)
+        new_DFG=[]
+        for d in DFG:
+            if d[1] in indexs:
+                new_DFG.append(d)
+        dfg=new_DFG
+    except:
+        dfg=[]
+    return code_tokens,dfg
